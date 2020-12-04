61a24887c5d5ce40ed62d8a1516966277e63dbea
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2020-12-04 16:00:49 -0600

    partD rename -> partB

--- partB/server/src/server.ts
+++ partB/server/src/server.ts
@@ -19,7 +19,7 @@ const HOST = '0.0.0.0';
 // ######################################################################################################################################
 
 // Database Assignment with table posts
-const container: string = 'dbPartD';
+const container: string = 'dbPartB';
 // const container: string = 'localhost';
 const database: string = 'coffeeShop';
 mongoose
