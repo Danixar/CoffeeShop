bd19a3f9c8c0d0d6756eaa2c6c9302f806c66bb6
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2020-12-22 19:07:47 -0600

    reorganizing partC's server

--- partC/server/server.ts
+++ partC/server/server.ts
@@ -7,30 +7,11 @@
 import { Application, Context } from 'https://deno.land/x/oak/mod.ts';
 import router from './routes.ts';
 import { oakCors } from 'https://deno.land/x/cors/mod.ts';
-import { MongoClient, Bson } from 'https://deno.land/x/mongo/mod.ts';
-
-// import express, { Application, Request, Response, NextFunction } from 'express';
-// import mongoose from 'mongoose';
-// import bodyParser from 'body-parser';
-// import { menu, users, orders } from './schemas';
-// import * as bcrypt from 'bcrypt';
 
 // Port and Host
 const PORT = 5000;
 const HOST = '0.0.0.0';
 const browserPort = 8000;
-const databaseHost = 27017;
-
-// ######################################################################################################################################
-
-// Database Assignment with table posts
-// const container = 'dbPartC';
-const container = 'localhost';
-const database = 'coffeeShop';
-const client = new MongoClient();
-await client.connect(`mongodb://${container}:${databaseHost}`);
-console.log(`Connected to MongoDB Database ${database} on ${container}`);
-const db = client.database(database);
 
 // ######################################################################################################################################
 
