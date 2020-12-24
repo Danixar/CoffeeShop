7eaacf48e590de445f7f6e09b484a527ae846be4
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2020-12-23 23:46:39 -0600

    removed react from partC

--- partC/browser/src/components/Lockout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-export const Lockout: React.FC = () => {
-	return (
-		<div className="container">
-			<h2>You do not have permission to view this page. Please login with the appropriate credentials.</h2>
-		</div>
-	);
-};
