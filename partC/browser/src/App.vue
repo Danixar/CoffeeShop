c98c82d960d93cc326b5fd0d5a49bcb70ea23b3d
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2020-12-26 20:04:00 -0600

    added simple vue routing

--- partC/browser/src/App.vue
+++ partC/browser/src/App.vue
@@ -1,30 +1,74 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+	<div id="app">
+		<Header />
+		<div id="navbar">
+			<router-link to="/"><button class="button">Home</button></router-link>
+			<router-link to="/about"><button class="button">Customer Portal</button></router-link>
+			<router-link to="/about"><button class="button">Employee Portal</button></router-link>
+			<router-link to="/about"><button class="button">Login</button></router-link>
+		</div>
+		<router-view />
+	</div>
 </template>
 
+<script lang="ts">
+import Header from '@/components/Header.vue';
+export default {
+	name: 'app',
+	components: { Header },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+	background: rgb(241, 241, 241);
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 16;
+	line-height: 1.6em;
+	margin: 0;
 }
 
-#nav {
-  padding: 30px;
+.container {
+	width: 80%;
+	margin: auto;
+	overflow: hidden;
+}
+.button {
+	height: 30px;
+	width: 250px;
+	background-color: white;
+	color: black;
+	border: 2px solid rgb(32, 178, 170);
+	transition-duration: 0.4s;
+	margin: 5px;
+	padding: 3px;
+}
+.button:hover {
+	background-color: rgb(32, 178, 170);
+	color: white;
+	box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#navbar {
+	position: sticky;
+	top: 9%;
+	background-color: rgb(255, 165, 0);
+	text-align: center;
+	padding: 7px;
+}
+#navbar a {
+	color: rgb(19, 19, 102);
+	text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+@media (max-width: 600px) {
+	#about {
+		width: 100%;
+		float: none;
+	}
+	#sidebar {
+		width: 100%;
+		float: none;
+	}
 }
 </style>
---
+++ partC/browser/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<header id="main-header">
+			<div class="container">
+				<h1>Coffee Shop</h1>
+			</div>
+		</header>
+	</div>
+</template>
+
+<script>
+export default { name: 'Header' };
+</script>
+
+<style>
+#main-header {
+	text-align: center;
+	background-color: rgb(32, 178, 170);
+	color: black;
+	padding: 15px;
+	font-size: 26px;
+	position: sticky;
+	top: 0;
+}
+</style>
